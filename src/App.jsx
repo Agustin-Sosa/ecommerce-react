ce9f4b83451b494924577d9241857a018c9c09f4
Agustin-Sosa <agustinomarsosa@gmail.com>
2024-10-10 18:50:28 -0300

    pre-entrega 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Footer } from "./components/layout/footer/Footer";
 import { Navbar } from "./components/layout/navbar/Navbar";
+import { ItemListContainer } from "./components/pages/itemListContainer/itemListContainer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ItemListContainer greeting="BIENVENIDO A FORESTALES BUFALO" />
       <Footer />
     </>
   );
---
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+// import { Card } from "../../common/card/Card";
+
+export const ItemListContainer = ({ greeting }) => {
+  return (
+    <div>
+      {/* <Card title="Leña algarrobo" price={4500} stock={10} />
+      <Card title="Leña quebracho blanco" price={4500} stock={15} />
+      <Card title="Leña quebracho colorado" price={5000} stock={5} />
+      <Card title="Carbón" price={4500} stock={8} /> */}
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
